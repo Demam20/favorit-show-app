9a8bd05c02fb4f678b97d401b34e81e3ef113451
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-02-20 07:24:02 -0800

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { CurrentShowComponent } from './current-show/current-show.component';
 import {ShowService} from './show.service'
 import {HttpClientModule} from '@angular/common/http';
+import { ShowSearchComponent } from './show-search/show-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentShowComponent
+    CurrentShowComponent,
+    ShowSearchComponent
   ],
   imports: [
     BrowserModule,
